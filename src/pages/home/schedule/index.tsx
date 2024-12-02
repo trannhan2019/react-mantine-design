723433bf7f06beab147f24edd744e0f3619fc4ac
trannhan2019 <53633928+trannhan2019@users.noreply.github.com>
2024-12-02 13:15:59 +0700

    02.12.2024-sba

--- src/pages/home/schedule/index.tsx
+++ src/pages/home/schedule/index.tsx
@@ -1,85 +1,76 @@
 import { Container } from "@mantine/core";
+import {
+  IconAmbulance,
+  IconArrowRight,
+  IconClock,
+  IconPrescription,
+} from "@tabler/icons-react";
+import React from "react";
 
 export default function Schedule() {
   return (
     <section className="schedule h-[230px] bg-white">
       <Container size="lg">
-        <div className="relative grid -translate-y-1/2 grid-cols-1 gap-7 md:grid-cols-2 lg:grid-cols-3">
+        <div className="relative grid grid-cols-1 gap-7 md:-translate-y-1/2 md:grid-cols-2 lg:grid-cols-3">
           <div>
-            {/* single-schedule */}
-            <div className="rounded-md bg-blue-500 p-8 transition-all duration-300 hover:-translate-y-1 hover:drop-shadow-2xl">
-              <div className="inner">
-                <div className="icon">
-                  <i className="fa fa-ambulance" />
-                </div>
-                <div className="single-content">
-                  <span>Lorem Amet</span>
-                  <h4>Emergency Cases</h4>
-                  <p>
-                    Lorem ipsum sit amet consectetur adipiscing elit. Vivamus et
-                    erat in lacus convallis sodales.
-                  </p>
-                  <a href="#">
-                    LEARN MORE
-                    <i className="fa fa-long-arrow-right" />
-                  </a>
-                </div>
-              </div>
-            </div>
+            <SingleSchedule
+              spanTag="Available Now"
+              h3Tag="Doctors Timetable"
+              pTag="Lorem ipsum sit amet consectetur adipiscing elit. Vivamus et erat in lacus convallis sodales"
+              icon={<IconAmbulance size={150} stroke={2} />}
+            />
           </div>
           <div>
-            {/* single-schedule */}
-            <div className="rounded-md bg-blue-500 p-8">
-              <div className="inner">
-                <div className="icon">
-                  <i className="icofont-prescription" />
-                </div>
-                <div className="single-content">
-                  <span>Fusce Porttitor</span>
-                  <h4>Doctors Timetable</h4>
-                  <p>
-                    Lorem ipsum sit amet consectetur adipiscing elit. Vivamus et
-                    erat in lacus convallis sodales.
-                  </p>
-                  <a href="#">
-                    LEARN MORE
-                    <i className="fa fa-long-arrow-right" />
-                  </a>
-                </div>
-              </div>
-            </div>
+            <SingleSchedule
+              spanTag="Fusce Porttitor"
+              h3Tag="Doctors Timetable"
+              pTag="Lorem ipsum sit amet consectetur adipiscing elit. Vivamus et
+                    erat in lacus convallis sodales."
+              icon={<IconPrescription size={150} stroke={2} />}
+            />
           </div>
           <div className="md:col-span-2 lg:col-span-1">
-            {/* single-schedule */}
-            <div className="rounded-md bg-blue-500 p-8">
-              <div className="inner">
-                <div className="icon">
-                  <i className="icofont-ui-clock" />
-                </div>
-                <div className="single-content">
-                  <span>Donec luctus</span>
-                  <h4>Opening Hours</h4>
-                  <ul className="time-sidual">
-                    <li className="day">
-                      Monday - Fridayp <span>8.00-20.00</span>
-                    </li>
-                    <li className="day">
-                      Saturday <span>9.00-18.30</span>
-                    </li>
-                    <li className="day">
-                      Monday - Thusday <span>9.00-15.00</span>
-                    </li>
-                  </ul>
-                  <a href="#">
-                    LEARN MORE
-                    <i className="fa fa-long-arrow-right" />
-                  </a>
-                </div>
-              </div>
-            </div>
+            <SingleSchedule
+              spanTag="Donec luctus"
+              h3Tag="Opening Hours"
+              pTag="Lorem ipsum sit amet consectetur adipiscing elit. Vivamus et erat in lacus convallis sodales"
+              icon={<IconClock size={150} stroke={2} />}
+            />
           </div>
         </div>
       </Container>
     </section>
   );
 }
+
+type ScheduleProps = {
+  spanTag: string;
+  h3Tag: string;
+  pTag: string;
+  icon: React.ReactNode;
+};
+
+const SingleSchedule = ({ spanTag, h3Tag, pTag, icon }: ScheduleProps) => {
+  return (
+    <div className="bg-primary overflow-hidden rounded-md p-8 text-white transition-all duration-300 hover:-translate-y-1 hover:drop-shadow-2xl">
+      <div className="relative pr-20">
+        <div className="absolute -bottom-16 -right-1/4 text-gray-300">
+          {icon}
+        </div>
+        <div className="single-content">
+          <span className="font-semibold">{spanTag}</span>
+          <h4 className="my-4 text-2xl font-semibold">{h3Tag}</h4>
+          <p className="mb-6 font-semibold">{pTag}</p>
+          <a
+            href="#"
+            className="group relative inline-flex items-center gap-2 font-semibold"
+          >
+            LEARN MORE
+            <IconArrowRight size={20} />
+            <span className="absolute bottom-0 h-0.5 w-0 bg-white transition-all duration-300 group-hover:w-full" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/home/slider/index.tsx
+++ src/pages/home/slider/index.tsx
@@ -28,9 +28,9 @@ export default function Slider() {
   const autoplay = useRef(Autoplay({ delay: 4000, stopOnMouseEnter: true }));
 
   return (
-    <section className="slider">
+    <section className="slider h-[380px] md:h-[600px]">
       <Carousel
-        height={600}
+        height={"100%"}
         loop={true}
         plugins={[autoplay.current]}
         controlSize={50}
@@ -47,148 +47,62 @@ export default function Slider() {
           },
         }}
       >
-        <Carousel.Slide pos={"relative"}>
-          <div className="absolute w-full">
-            <Container size="lg">
-              <div className="w-7/12">
-                <motion.h1
-                  className="text-4xl font-bold text-slate-800"
-                  variants={fadeUp(200, 100)}
-                  initial="hidden"
-                  whileInView="show"
-                >
-                  We Provide <span className="text-sky-600">Medical</span>{" "}
-                  Services That You Can{" "}
-                  <span className="text-sky-600">Trust!</span>
-                </motion.h1>
+        <SingleSlider imgUrl={ImgSlide1} />
 
-                <motion.p
-                  variants={fadeUp(200, 110, 0.7)}
-                  initial="hidden"
-                  whileInView="show"
-                  className="text-slate-700"
-                >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Mauris sed nisl pellentesque, faucibus libero eu, gravida
-                  quam.
-                </motion.p>
+        <SingleSlider imgUrl={ImgSlide2} />
 
-                <motion.div
-                  variants={fadeUp(100, 150, 0.8)}
-                  initial="hidden"
-                  whileInView="show"
-                  className="flex gap-3"
-                >
-                  <Button className="group relative" size="lg">
-                    <span className="absolute left-0 h-full w-0 bg-gray-800 transition-all duration-300 ease-in-out group-hover:w-[50%]" />
-                    <span className="z-10">Get Appointment</span>
-                    <span className="absolute right-0 h-full w-0 bg-gray-800 transition-all duration-300 ease-in-out group-hover:w-[50%]" />
-                  </Button>
-                  <Button size="lg" bg="black">
-                    Learn More
-                  </Button>
-                </motion.div>
-              </div>
-            </Container>
-          </div>
-
-          <Image src={ImgSlide1} />
-        </Carousel.Slide>
-
-        <Carousel.Slide pos={"relative"}>
-          <div className="absolute w-full">
-            <Container size="lg">
-              <div className="w-7/12">
-                <motion.h1
-                  className="text-4xl font-bold text-slate-800"
-                  variants={fadeUp(200, 100)}
-                  initial="hidden"
-                  whileInView="show"
-                >
-                  We Provide <span className="text-sky-600">Medical</span>{" "}
-                  Services That You Can{" "}
-                  <span className="text-sky-600">Trust!</span>
-                </motion.h1>
-
-                <motion.p
-                  variants={fadeUp(200, 110, 0.7)}
-                  initial="hidden"
-                  whileInView="show"
-                  className="text-slate-700"
-                >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Mauris sed nisl pellentesque, faucibus libero eu, gravida
-                  quam.
-                </motion.p>
-
-                <motion.div
-                  variants={fadeUp(100, 150, 0.8)}
-                  initial="hidden"
-                  whileInView="show"
-                  className="flex gap-3"
-                >
-                  <Button className="group relative" size="lg">
-                    <span className="absolute left-0 h-full w-0 bg-gray-800 transition-all duration-300 ease-in-out group-hover:w-[50%]" />
-                    <span className="z-10">Get Appointment</span>
-                    <span className="absolute right-0 h-full w-0 bg-gray-800 transition-all duration-300 ease-in-out group-hover:w-[50%]" />
-                  </Button>
-                  <Button size="lg" bg="black">
-                    Learn More
-                  </Button>
-                </motion.div>
-              </div>
-            </Container>
-          </div>
-          <Image src={ImgSlide2} />
-        </Carousel.Slide>
-
-        <Carousel.Slide pos={"relative"}>
-          <div className="absolute w-full">
-            <Container size="lg">
-              <div className="w-7/12">
-                <motion.h1
-                  className="text-4xl font-bold text-slate-800"
-                  variants={fadeUp(200, 100)}
-                  initial="hidden"
-                  whileInView="show"
-                >
-                  We Provide <span className="text-sky-600">Medical</span>{" "}
-                  Services That You Can{" "}
-                  <span className="text-sky-600">Trust!</span>
-                </motion.h1>
-
-                <motion.p
-                  variants={fadeUp(200, 110, 0.7)}
-                  initial="hidden"
-                  whileInView="show"
-                  className="text-slate-700"
-                >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Mauris sed nisl pellentesque, faucibus libero eu, gravida
-                  quam.
-                </motion.p>
-
-                <motion.div
-                  variants={fadeUp(100, 150, 0.8)}
-                  initial="hidden"
-                  whileInView="show"
-                  className="flex gap-3"
-                >
-                  <Button className="group relative" size=" lg">
-                    <span className="absolute left-0 h-full w-0 bg-gray-800 transition-all duration-300 ease-in-out group-hover:w-[50%]" />
-                    <span className="z-10">Get Appointment</span>
-                    <span className="absolute right-0 h-full w-0 bg-gray-800 transition-all duration-300 ease-in-out group-hover:w-[50%]" />
-                  </Button>
-                  <Button size="lg" bg="black">
-                    Learn More
-                  </Button>
-                </motion.div>
-              </div>
-            </Container>
-          </div>
-          <Image src={ImgSlide3} />
-        </Carousel.Slide>
+        <SingleSlider imgUrl={ImgSlide3} />
       </Carousel>
     </section>
   );
 }
+
+const SingleSlider = ({ imgUrl }: { imgUrl: string }) => {
+  return (
+    <Carousel.Slide pos={"relative"}>
+      <div className="absolute h-full w-full">
+        <Container size="lg">
+          <div className="w-full md:w-7/12">
+            <motion.h1
+              className="text-2xl font-bold text-slate-800 md:text-4xl"
+              variants={fadeUp(100, 50)}
+              initial="hidden"
+              whileInView="show"
+            >
+              We Provide <span className="text-sky-600">Medical</span> Services
+              That You Can <span className="text-sky-600">Trust!</span>
+            </motion.h1>
+
+            <motion.p
+              variants={fadeUp(110, 55, 0.7)}
+              initial="hidden"
+              whileInView="show"
+              className="text-slate-700"
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sed nisl pellentesque, faucibus libero eu, gravida quam.
+            </motion.p>
+
+            <motion.div
+              variants={fadeUp(100, 80, 0.8)}
+              initial="hidden"
+              whileInView="show"
+              className="flex gap-3"
+            >
+              <Button className="group relative" size="lg">
+                <span className="absolute left-0 h-full w-0 bg-gray-800 transition-all duration-300 ease-in-out group-hover:w-[50%]" />
+                <span className="z-10">Get Appointment</span>
+                <span className="absolute right-0 h-full w-0 bg-gray-800 transition-all duration-300 ease-in-out group-hover:w-[50%]" />
+              </Button>
+              <Button size="lg" bg="black">
+                Learn More
+              </Button>
+            </motion.div>
+          </div>
+        </Container>
+      </div>
+
+      <Image src={imgUrl} />
+    </Carousel.Slide>
+  );
+};
